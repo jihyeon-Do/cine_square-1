4f2519f20185cecae33e2a41216ec065ba1ea2a4
jihyeon57 <ehwlgus22@gmail.com>
2021-07-17 03:50:07 +0900

    signin component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function Home() {
+export default function Home({ history }) {
+  function goLogin() {
+    history.push('/signin')
+  }
   return (
     <div>
       <h1>홈</h1>
+      <button onClick={goLogin}>로그인</button>
     </div>
   );
 }
