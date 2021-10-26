a839decc27f1d36ffa4ef12fc1a108eb916ef3ea
jihyeon57 <ehwlgus22@gmail.com>
2021-10-26 18:12:52 +0900

    add boxoffice saga

--- src/redux/middleware/saga.js
+++ src/redux/middleware/saga.js
@@ -1,6 +1,7 @@
 import { all } from 'redux-saga/effects';
 import { getSearchValueSaga } from '../modules/search';
+import { getBoxOfficeListSaga } from '../modules/boxoffice';
 
 export default function* rootSaga() {
-  yield all([getSearchValueSaga()]);
+  yield all([getSearchValueSaga(), getBoxOfficeListSaga()]);
 }
