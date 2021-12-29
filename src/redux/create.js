91a846a34bcb8313dcd964b868c126195dc400bb
jihyeon57 <ehwlgus22@gmail.com>
2021-12-30 03:55:00 +0900

    Maintain login session on reload

--- src/redux/create.js
+++ src/redux/create.js
@@ -3,6 +3,9 @@ import rootReducer from './modules/index';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import { routerMiddleware } from 'connected-react-router';
+import rootSaga from './middleware/saga';
+import TokenService from '../service/TokenService';
+import UserService from '../service/UserService';
 // const store = createStore(rootReducer);
 
 // export const history = createBrowserHistory();
@@ -12,10 +15,19 @@ export const sagaMiddleware = createSagaMiddleware();
 export default function create(history) {
   const store = createStore(
     rootReducer(history),
+    {
+      auth: {
+        token: TokenService.get(),
+        loading: false,
+        error: null,
+      },
+    },
     composeWithDevTools(
       applyMiddleware(routerMiddleware(history), sagaMiddleware),
     ),
   );
+  sagaMiddleware.run(rootSaga);
+
   return store;
 }
 
