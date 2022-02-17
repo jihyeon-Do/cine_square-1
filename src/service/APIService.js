edb2e742d80154716c8af7787ece6ba1882a6474
jihyeon57 <ehwlgus22@gmail.com>
2022-02-17 17:41:05 +0900

    API port 변경

--- src/service/APIService.js
+++ src/service/APIService.js
@@ -1,4 +1,4 @@
 export default class APIService {
-  static AWSAPI = 'http://cinesquare.yahmedora.com:9696';
+  static AWSAPI = 'http://cinesquare.yahmedora.com:9096';
   static LOCALAPI = 'http://192.168.104.90:8080';
 }
