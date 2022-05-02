62ca33aa6f01cd71031703b46e718cc783a3300b
jihyeon57 <ehwlgus22@gmail.com>
2022-05-02 16:26:22 +0900

    👽️ Chore: API 주소 변경

--- src/service/APIService.js
+++ src/service/APIService.js
@@ -1,5 +1,5 @@
 export default class APIService {
   static PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
-  static AWSAPI = 'http://cinesquare.yahmedora.com:9096';
+  static AWSAPI = 'http://cinesquare.slowtuttle.co.kr:9096';
   static LOCALAPI = 'http://192.168.104.90:8080';
 }
